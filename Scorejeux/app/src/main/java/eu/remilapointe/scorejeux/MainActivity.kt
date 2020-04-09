31348d6f4bef3a61c3bc8222ba45adf34e8c8c6a
lapoint1 <remi.lapointe@nokia.com>
2020-04-09 22:38:26 +0200

    Some modif in ScoreJeux project

--- Scorejeux/app/src/main/java/eu/remilapointe/scorejeux/MainActivity.kt
+++ Scorejeux/app/src/main/java/eu/remilapointe/scorejeux/MainActivity.kt
@@ -7,12 +7,32 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.log4k.DefaultAppender
+import com.log4k.Level
+import com.log4k.Log4k
+import com.log4k.android.AndroidAppender
+import java.io.File
+import java.io.PrintWriter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (BuildConfig.DEBUG) {
+            Log4k.add(Level.Verbose, ".*", AndroidAppender())
+            Log4k.add(Level.Verbose, "com\\.log4k\\.sample\\..+", DefaultAppender())
+            Log4k.add(Level.Verbose, ".*", DefaultAppender(writer = PrintWriter(File(externalCacheDir, "debug-log.txt"))))
+        } else {
+            Log4k.add(
+                Level.Assert, "com\\.log4k\\.sample\\..+", DefaultAppender(writer = PrintWriter(
+                    File(filesDir, "log.txt")
+                )
+                )
+            )
+        }
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
